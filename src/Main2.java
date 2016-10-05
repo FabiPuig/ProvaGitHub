8145571e686cec5861d477f320655cf251284d50
Fabian <20464654j@iespoblenou.org>
2016-10-05 18:33:59 +0200

    "commit 1"

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+/**
+ * Created by 20464654j on 05/10/16.
+ */
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("prova 2");
+        hello
+
+    }
+}
